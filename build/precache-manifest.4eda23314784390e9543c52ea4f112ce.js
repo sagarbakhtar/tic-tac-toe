1426e3de7c6f9b33e6aeee5815dffe5f0973704b
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-03-01 19:47:44 +0530

    Build

---
+++ build/precache-manifest.4eda23314784390e9543c52ea4f112ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "f7c778a503a9d1e14b8f",
+    "url": "/tic-tac-toe/static/js/main.f7c778a5.chunk.js"
+  },
+  {
+    "revision": "958d2263acedb09ac06c",
+    "url": "/tic-tac-toe/static/js/1.958d2263.chunk.js"
+  },
+  {
+    "revision": "f7c778a503a9d1e14b8f",
+    "url": "/tic-tac-toe/static/css/main.a86e79b2.chunk.css"
+  },
+  {
+    "revision": "57b5093306ff02fd70f9cd1c91856e6b",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
